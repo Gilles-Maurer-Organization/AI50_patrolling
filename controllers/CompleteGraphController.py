6108cec870a4b67921996c81f700edef87f7e733
gilles-maurer <104887265+gilles-maurer@users.noreply.github.com>
2024-10-26 12:12:04 +0200

    finish A* and complete graph generation

--- controllers/CompleteGraphController.py
+++ controllers/CompleteGraphController.py
@@ -12,6 +12,7 @@ import pygame
 <<<<<<< HEAD
 =======
 import pygame
+<<<<<<< HEAD
 >>>>>>> e6b0877 (ajout complete graph controller)
 from models import Graph
 from views import GraphView
@@ -21,15 +22,20 @@ from controllers.CSVController import CSVController
 <<<<<<< HEAD
 >>>>>>> 4516dea (ajout complete graph controller)
 =======
+=======
+>>>>>>> 033ea83 (finish A* and complete graph generation)
 # from models import Graph
 # from views import GraphView
 # from controllers.NodeController import NodeController
 # from controllers.EdgeController import EdgeController
 # from controllers.CSVController import CSVController
 import math
+<<<<<<< HEAD
 >>>>>>> 1e5a6d3 (finish A* and complete graph generation)
 =======
 >>>>>>> e6b0877 (ajout complete graph controller)
+=======
+>>>>>>> 033ea83 (finish A* and complete graph generation)
 
 
 class CompleteGraphController:
@@ -40,6 +46,7 @@ class CompleteGraphController:
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 
             nodePosition : dictionnaire qui pour chaque sommet indique ses coordonnées
 
@@ -50,6 +57,9 @@ class CompleteGraphController:
 >>>>>>> 1e5a6d3 (finish A* and complete graph generation)
 =======
 >>>>>>> e6b0877 (ajout complete graph controller)
+=======
+            nodePosition : dictionnaire qui pour chaque sommet indique ses coordonnées
+>>>>>>> 033ea83 (finish A* and complete graph generation)
             simpleGraph : la matrice représentant les distances entre les sommets du graph simple 
             completeGraph : la matrice représentant les distances entre les sommets du graph complet 
 
@@ -87,16 +97,24 @@ class CompleteGraphController:
     
     '''
      
-    def __init__(self, simpleGraph) :
+    def __init__(self, simpleGraph, nodePosition): 
 
         self.simpleGraph = simpleGraph
+<<<<<<< HEAD
         self.completeGraph, self.shortestWayMatrix = self.create_complete_graph()
 >>>>>>> e6b0877 (ajout complete graph controller)
+=======
+        self.nodePosition = nodePosition
+        self.completeGraph = [[0 for i in range(len(simpleGraph))] for j in range(len(simpleGraph))]
+        self.shortestWayMatrix = [[[] for i in range(len(simpleGraph))] for j in range(len(simpleGraph))]
+        self.create_complete_graph()
+>>>>>>> 033ea83 (finish A* and complete graph generation)
 
         return
     
     def create_complete_graph(self): 
 
+<<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
@@ -110,6 +128,8 @@ class CompleteGraphController:
         # TODO : algo qui crée le graph complet 
 
 >>>>>>> e6b0877 (ajout complete graph controller)
+=======
+>>>>>>> 033ea83 (finish A* and complete graph generation)
         # exemple 
         # simple :  [[0, 1, 0], [1, 0, 1], [0, 1, 0]]
         # complet : [[0, 1, 2], [1, 0, 1], [2, 1, 0]]
@@ -124,8 +144,11 @@ class CompleteGraphController:
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
 >>>>>>> 1e5a6d3 (finish A* and complete graph generation)
+=======
+>>>>>>> 033ea83 (finish A* and complete graph generation)
                 if self.simpleGraph[i][j] == 0:
 
                     aStar = AStarAlgorithm(self.simpleGraph, self.nodePosition, i, j)
@@ -146,6 +169,7 @@ class CompleteGraphController:
                     self.shortestWayMatrix[j][i] = [j, i]
                     
 <<<<<<< HEAD
+<<<<<<< HEAD
 
 =======
 =======
@@ -161,6 +185,9 @@ class CompleteGraphController:
 >>>>>>> 1e5a6d3 (finish A* and complete graph generation)
 =======
 >>>>>>> e6b0877 (ajout complete graph controller)
+=======
+
+>>>>>>> 033ea83 (finish A* and complete graph generation)
 
     def get_shortest_way(self, node1, node2): 
 
@@ -330,4 +357,115 @@ if __name__ == "__main__":
 >>>>>>> 1e5a6d3 (finish A* and complete graph generation)
 =======
 
+<<<<<<< HEAD
 >>>>>>> e6b0877 (ajout complete graph controller)
+=======
+class AStarAlgorithm: 
+
+    def __init__(self, graph, nodePosition, start, end): 
+        self.graph = graph
+        self.nodePosition = nodePosition
+        self.start = start
+        self.end = end
+
+        self.openSet = []
+        self.camemFrom = {}
+        self.gScore = {}
+        self.fScore = {}
+
+        for node in self.nodePosition.keys():
+            self.gScore[node] = math.inf
+            self.fScore[node] = math.inf
+
+    def compute_h (self, node): 
+
+        # distance entre le noeud et la fin
+        (x1, y1) = self.nodePosition[node]
+        (x2, y2) = self.nodePosition[self.end]
+
+        return math.sqrt((x1 - x2)**2 + (y1 - y2)**2)
+    
+    def reconstruct_path(self, current): 
+        total_path = [current]
+        while current in self.camemFrom.keys(): 
+            current = self.camemFrom[current]
+            total_path.insert(0, current)
+
+        return total_path
+    
+
+    def a_star(self):
+
+        openSet = [self.start]  
+
+        self.gScore[self.start] = 0 
+        self.fScore[self.start] = self.compute_h(self.start)
+
+        while len(openSet) > 0:
+
+            current = openSet[0]
+            for node in openSet: 
+                if self.fScore[node] < self.fScore[current]: 
+                    current = node
+
+            if current == self.end: 
+                return (self.reconstruct_path(current), self.gScore[current])
+
+            openSet.remove(current)
+
+            for i in range(len(self.graph[current])): 
+                if self.graph[current][i] > 0: 
+                    tentative_gScore = self.gScore[current] + self.graph[current][i]
+
+                    if tentative_gScore < self.gScore[i]: 
+                        self.camemFrom[i] = current
+                        self.gScore[i] = tentative_gScore
+                        self.fScore[i] = tentative_gScore + self.compute_h(i)
+                        if i not in openSet: 
+                            openSet.append(i)
+
+        return None
+                
+                
+
+def main():
+    # Exemple de graphe simple avec 4 nœuds (graphe non complet)
+    # 0 est connecté à 1 avec une distance de 1, et 1 est connecté à 2 avec une distance de 2
+    simple_graph = [
+        [0, 1, 4, 0, 0, 0],  # Node 0
+        [1, 0, 2, 5, 0, 0],  # Node 1
+        [4, 2, 0, 1, 3, 0],  # Node 2
+        [0, 5, 1, 0, 2, 6],  # Node 3
+        [0, 0, 3, 2, 0, 2],  # Node 4
+        [0, 0, 0, 6, 2, 0]   # Node 5
+    ]
+
+    # Position des nœuds (x, y)
+    node_positions = {
+        0: (0, 0),
+        1: (1, 0),
+        2: (2, 1),
+        3: (3, 0),
+        4: (2, -1),
+        5: (3, -1)
+    }
+
+    # Initialisation du contrôleur de graphe complet
+    graph_controller = CompleteGraphController(simple_graph, node_positions)
+    complete_graph = graph_controller.get_complete_graph()
+    print("Graphe complet généré :")
+    for row in complete_graph:
+        print(row)
+
+
+    # Tester A* pour obtenir le plus court chemin entre chaque paire de nœuds
+    for start in range(len(simple_graph)):
+        for end in range(len(simple_graph)):
+            if start != end:
+                astar = AStarAlgorithm(graph=simple_graph, nodePosition=node_positions, start=start, end=end)
+                shortest_path = astar.a_star()
+                print(f"Chemin le plus court de {start} à {end} : {shortest_path}")
+
+if __name__ == "__main__":
+    main()
+>>>>>>> 033ea83 (finish A* and complete graph generation)
