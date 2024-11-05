284c189dd3209ef5591837fa0a5a65ff4bf099d6
Raphaël PERRIN <raphael.perrin754@gmail.com>
2024-11-05 08:48:22 +0100

    feat: continuing ScrollingListController unit tests

--- tests/test_ScrollingListController.py
+++ tests/test_ScrollingListController.py
@@ -25,19 +25,24 @@ class TestScrollingListController(unittest.TestCase):
         self.scrolling_list_controller.scrolling_list_view.draw.assert_called_once()
     
     def test_is_scrolling_list_header_hovered_scrolling_list_doesnt_exist(self):
-        event = pygame.event.Event(pygame.MOUSEBUTTONDOWN, {"pos": (30, 30)})
-        self.scrolling_list_controller.scrolling_list_view.scrolling_list_rect = Mock()
+        event = pygame.event.Event(pygame.MOUSEBUTTONDOWN, {"pos": (30 + GRAPH_WINDOW_WIDTH, 30)})
+        self.scrolling_list_controller.scrolling_list_view.scrolling_list_rect = pygame.Rect(0, 0, 0, 0)
         is_hovered = self.scrolling_list_controller.is_scrolling_list_header_hovered(event)
 
-        #self.assertEqual(is_hovered, True)
-    '''
+        self.assertFalse(is_hovered)
+    
     def test_is_scrolling_list_header_hovered_is_not_hovered(self):
-        event = pygame.event.Event(pygame.MOUSEBUTTONDOWN, {"pos": (30, 30)})
-        self.scrolling_list_controller.is_scrolling_list_header_hovered(event)
+        event = pygame.event.Event(pygame.MOUSEBUTTONDOWN, {"pos": (30 + GRAPH_WINDOW_WIDTH, 30)})
+        is_hovered = self.scrolling_list_controller.is_scrolling_list_header_hovered(event)
+
+        self.assertFalse(is_hovered)
 
     def test_is_scrolling_list_header_hovered_is_hovered(self):
-        event = pygame.event.Event(pygame.MOUSEBUTTONDOWN, {"pos": (30, 30)})
-        self.scrolling_list_controller.is_scrolling_list_header_hovered(event)
+        event = pygame.event.Event(pygame.MOUSEBUTTONDOWN, {"pos": (30 + GRAPH_WINDOW_WIDTH, 30)})
+        self.scrolling_list_controller.scrolling_list_view.scrolling_list_rect = pygame.Rect(20, 20, 20, 20)
+        is_hovered = self.scrolling_list_controller.is_scrolling_list_header_hovered(event)
+
+        self.assertTrue(is_hovered)
     
     def test_handle_event(self):
         pass
@@ -53,5 +58,3 @@ class TestScrollingListController(unittest.TestCase):
 
     def test_get_selected_algorithm(self):
         pass
-
-    '''
