2f728e3ba7f83c063bb5193cd68b8e7f5891d78d
Arnaud Planchin <arnaud.planchin@laposte.net>
2024-11-19 15:51:36 +0100

    Algo glouton

--- controllers/naive_algorithm/naive.py
+++ controllers/naive_algorithm/naive.py
@@ -2,17 +2,17 @@ import numpy as np
 import random as rd
 
 class Naive:
-    def __init__(self):
+    def __init__(self,nb_agent,distance_matrix):
+        self.nb_agent = nb_agent
+        self.distance_matrix = distance_matrix
         pass
     
     
-    def naive_shortest_path(self,distance_matrix):
+    def naive_shortest_path(self):
         
-        nb_nodes = distance_matrix.shape[0]
+        nb_nodes = self.distance_matrix.shape[0]
         visited = [False] * nb_nodes
         path = []
-        
-        #Ajouter le test pour s'executer tant que le chemin ne respecte pas les liens entre les noeuds (car il ne passe pas par le même noeud)
 
         # Commencer à partir du nœud 0 (ou n'importe quel autre nœud)
         current_node = rd.randint(0,nb_nodes-1)
@@ -21,29 +21,36 @@ class Naive:
         
         while len(path) < nb_nodes:
             # Trouver le nœud le plus proche non visité
-            next_node = np.argmin([distance_matrix[current_node][i] if not visited[i] else np.inf for i in range(nb_nodes)])
+            next_node = np.argmin([self.distance_matrix[current_node][i] if not visited[i] else np.inf for i in range(nb_nodes)])
             path.append(next_node)
             visited[next_node] = True
             current_node = next_node
 
+        path.append(path[0])
         
         return path
         
     
-    def launch(self,nb_agents,distance_matrix):
+    def launch(self):
 
         paths = []
         #a partir d'un graphe et du nombre d'agents
         #génére un chemin le plus court passant par tous les points et ne se répète pas
 
-        path = self.naive_shortest_path(distance_matrix)
+        path = self.naive_shortest_path()
 
-        for idx in range(nb_agents):
-            agent_start_index = int(len(path)/nb_agents)
+        #Rajouter les noeuds relier chacun des neouds du indicatif path
+        
+
+        if(self.nb_agent > 1):
+            for idx in range(self.nb_agent):
+                agent_start_index = int(len(path)/self.nb_agent)
+                paths.append(path.copy())
+                for i in range(agent_start_index):
+                    path.append(path[0])
+                    path.pop(0)
+        else :
             paths.append(path.copy())
-            for i in range(agent_start_index):
-                path.append(path[0])
-                path.pop(0)
         
         #retourne la liste des chemins de chaque agent
         return path,paths
@@ -51,7 +58,8 @@ class Naive:
 
 
 def main():
-    
+    nb_agent = 1
+
     coords = np.array([
         [0, 0],  
         [0, 1],  
@@ -70,13 +78,12 @@ def main():
         for j in range(nb_nodes):
             distance_matrix[i][j] = np.linalg.norm(coords[i] - coords[j])  # Distance euclidienne
     
+    #Matrice de liens entre chaque noeuds 
     
-    
-    naif = Naive()
-    
-    path,paths = naif.launch(4,distance_matrix)
-    print(path)
-    print(paths)
+    naif = Naive(nb_agent,distance_matrix)
+    path,paths = naif.launch()
+    print("Le chemin à parcourir : ", path)
+    print("Les chemins de chaque agent : ", paths)
 
 
 
