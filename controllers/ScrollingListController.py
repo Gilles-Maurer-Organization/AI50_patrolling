92fe0bc15e8d5beda186f96f76918150bef7814c
DynaTiuM <raphael.perrin754@gmail.com>
2024-12-01 20:45:32 +0100

    feat: finishing commenting controllers

--- controllers/ScrollingListController.py
+++ controllers/ScrollingListController.py
@@ -1,97 +1,142 @@
-from constants.Config import GRAPH_WINDOW_WIDTH
-
-from views.ScrollingListView import ScrollingListView
-
-from models.ScrollingList import ScrollingList
-
-from models.algorithms.AlgorithmModel import AlgorithmModel
-from models.algorithms.Naive import Naive
-from models.algorithms.Evolutional import Evolutional
-from models.algorithms.AntColony import AntColony
-
 import pygame
 
+from constants.Config import GRAPH_WINDOW_WIDTH
+
+from models.ScrollingList import ScrollingList
+from models.algorithms.AlgorithmModel import AlgorithmModel
+from models.algorithms.AntColony import AntColony
+from models.algorithms.Evolutional import Evolutional
+from models.algorithms.Naive import Naive
+from views.ParametersView import ParametersView
+from views.ScrollingListView import ScrollingListView
+
+
 class ScrollingListController:
-    def __init__(self, parameters_view) -> None:
-        self.parameters_view = parameters_view
-        self.scrolling_list = ScrollingList(
+    """
+    The ScrollingListController handles the logic and interaction of the scrolling list 
+    for selecting algorithms in the ParametersView.
+
+    Attributes:
+        _scrolling_list (ScrollingList): The model containing the list of algorithms and their states.
+        _scrolling_list_view (ScrollingListView): The view responsible for rendering the scrolling list.
+
+    Methods:
+        draw_scrolling_list():
+            Draws the scrolling list with available algorithms.
+
+        handle_event(event):
+            Handles user interactions such as mouse clicks or movements for controlling the scrolling list.
+
+        _is_scrolling_list_header_hovered(event):
+            Checks if the mouse is hovering over the header of the scrolling list.
+
+        _handle_header_clicked():
+            Toggles the scrolling list between active (expanded) and inactive (collapsed) states when the header is clicked.
+
+        _handle_header_hovered(event):
+            Changes the appearance of the scrolling list header when hovered by the mouse.
+
+        _handle_scrolling_list_options(event):
+            Handles the selection of an algorithm from the scrolling list and updates the state accordingly.
+
+        get_selected_algorithm():
+            Returns the currently selected algorithm from the scrolling list.
+    """
+    def __init__(self, parameters_view: ParametersView) -> None:
+        self._scrolling_list = ScrollingList(
             [Naive(), Evolutional(), AntColony()]
         )
-        self.scrolling_list_view = ScrollingListView(parameters_view.screen, 10, 110, 290, 40)
+        self._scrolling_list_view = ScrollingListView(parameters_view.screen, 10, 110, 290, 40)
 
-    def draw_scrolling_list(self):
-        '''
-        Cette méthode dessine la liste déroulante.
-        '''
-        algorithms = self.scrolling_list.get_algorithms()
-        selected_algorithm = self.scrolling_list.get_selected_algorithm()
+    def draw_scrolling_list(self) -> None:
+        """
+        Draws the scrolling list with available algorithms.
+        """
+        algorithms = self._scrolling_list.get_algorithms()
+        selected_algorithm = self._scrolling_list.get_selected_algorithm()
 
-        self.scrolling_list_view.draw(algorithms, selected_algorithm, self.scrolling_list.has_an_algorithm_selected())
+        self._scrolling_list_view.draw(algorithms, selected_algorithm, self._scrolling_list.has_an_algorithm_selected())
 
-    def is_scrolling_list_header_hovered(self, event):
-        '''
-        Cette méthode vérifie si la souris est située dans les limites de la liste déroulante.
+    def handle_event(self, event: pygame.event.Event) -> bool:
+        """
+        Checks if the mouse is hovering over the scrolling list header.
 
         Args:
-            event: L'événement Pygame contenant des informations sur les coordonnées de la souris.
-        '''
-        mouse_pos = (event.pos[0] - GRAPH_WINDOW_WIDTH, event.pos[1])
-        if self.scrolling_list_view.scrolling_list_rect:
-            return self.scrolling_list_view.scrolling_list_rect.collidepoint(mouse_pos)
-        return False
-
-    def handle_event(self, event) -> bool:
-        '''
-        Cette méthode gère les interactions de l'utilisateur avec la souris, permettant de contrôler 
-        le comportement de la liste déroulante (ouverture, sélection, etc.).
- 
-        Args:
-            event: L'événement Pygame contenant des informations sur les coordonnées de la souris.
-        '''
+            event: The Pygame event containing information about mouse position.
+        """
         if event.type == pygame.MOUSEBUTTONDOWN:
-            # Si l'élément est survolé, c'est que l'utilisateur a cliqué sur le header de la liste déroulante
-            if self.is_scrolling_list_header_hovered(event):
-                return self.handle_header_clicked()
+            # If the header is hovered, it means the user clicked on the scrolling list header
+            if self._is_scrolling_list_header_hovered(event):
+                return self._handle_header_clicked()
 
-            # Sinon, c'est qu'il essaie soit de cliquer sur les options de la liste déroulante, soit en dehors
-            elif self.scrolling_list_view.is_active:
-                return self.handle_scrolling_list_options(event)
+            # Otherwise, check if the user clicked on the scrolling list options or outside of it
+            elif self._scrolling_list_view.is_active:
+                return self._handle_scrolling_list_options(event)
         
-        # Si l'utilisateur essaie de hover le header de la liste déroulante
+         # If the user is trying to hover over the scrolling list header
         if event.type == pygame.MOUSEMOTION:
-            return self.handle_header_hovered(event)
+            return self._handle_header_hovered(event)
+        return False
+    
+    def _is_scrolling_list_header_hovered(self, event: pygame.event.Event) -> None:
+        """
+        This method checks if the mouse is within the bounds of the scrolling list.
+
+        Args:
+            event: The Pygame event containing information about the mouse's coordinates.
+        """
+        mouse_pos = (event.pos[0] - GRAPH_WINDOW_WIDTH, event.pos[1])
+        if self._scrolling_list_view.scrolling_list_rect:
+            return self._scrolling_list_view.scrolling_list_rect.collidepoint(mouse_pos)
         return False
 
-    def handle_header_clicked(self) -> bool:
-        # Si la liste déroulante est déjà déroulée:
-        if self.scrolling_list_view.is_active:
-            # On la replie
-            self.scrolling_list_view.set_active(False)
+    def _handle_header_clicked(self) -> bool:
+        """
+        Toggles the scrolling list between active (expanded) and inactive (collapsed) states.
+        """
+        # if the scrolling list is already expanded:
+        if self._scrolling_list_view.is_active:
+            # Collapse it
+            self._scrolling_list_view.set_active(False)
             return False
-        # Sinon, on la déplie pour faire apparaitre les options d'algorithmes
-        self.scrolling_list_view.set_active(True)
+        # Otherwise, expand the scrolling list to show algorithm options
+        self._scrolling_list_view.set_active(True)
     
-    def handle_header_hovered(self, event) -> bool:
-        if not self.scrolling_list_view.is_active:
-            if self.is_scrolling_list_header_hovered(event):
-                self.scrolling_list_view.set_hovered()
-            else:
-                self.scrolling_list_view.set_normal()
+    def _handle_header_hovered(self, event: pygame.event.Event) -> bool:
+        """
+        Changes the appearance of the scrolling list header when hovered by the mouse.
 
-    def handle_scrolling_list_options(self, event) -> bool:
-        self.scrolling_list_view.set_active(False)
-        # Récupération des options disponibles depuis le modèle et vérification que l'élément cliqué
-        # est un algorithme disponible dans la liste
-        selected_option = self.scrolling_list_view.is_option_clicked(event, self.scrolling_list.get_algorithms())
-        # S'il s'agit d'une option d'algorithme qui a été sélectionnée:
+        Args:
+            event: The Pygame event containing information about mouse position.
+        """
+        if not self._scrolling_list_view.is_active:
+            if self._is_scrolling_list_header_hovered(event):
+                self._scrolling_list_view.set_hovered()
+            else:
+                self._scrolling_list_view.set_normal()
+
+    def _handle_scrolling_list_options(self, event: pygame.event.Event) -> bool:
+        """
+        Handles the selection of an algorithm from the scrolling list.
+
+        Args:
+            event: The Pygame event containing information about mouse actions.
+        """
+        self._scrolling_list_view.set_active(False)
+        # Get available options from the model and check if a valid option was clicked
+        selected_option = self._scrolling_list_view.is_option_clicked(event, self._scrolling_list.get_algorithms())
+        # If a valid algorithm option was selected
         if selected_option:
-            self.scrolling_list.set_selected_algorithm(selected_option)
-            self.scrolling_list_view.set_active(False)
+            self._scrolling_list.set_selected_algorithm(selected_option)
+            self._scrolling_list_view.set_active(False)
             return True
         
-        # Sinon, c'est que l'utilisateur a cliqué en dehors de la liste d'options, on ferme la liste déroulante
-        self.scrolling_list_view.set_active(False)
+        # Otherwise, close the scrolling list if the user clicked outside
+        self._scrolling_list_view.set_active(False)
         return False
 
     def get_selected_algorithm(self) -> AlgorithmModel:
-        return self.scrolling_list.get_selected_algorithm()
+        """
+        Returns the currently selected algorithm from the scrolling list.
+        """
+        return self._scrolling_list.get_selected_algorithm()
