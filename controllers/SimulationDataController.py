58b6b7c21896877ad218edb83d149fef723858b5
DynaTiuM <raphael.perrin754@gmail.com>
2024-12-12 18:59:32 +0100

    fix: code architecture: view, controller & model of Simulation Data fixed

--- controllers/SimulationDataController.py
+++ controllers/SimulationDataController.py
@@ -1,11 +1,15 @@
 import pygame
 
+from constants.Config import GRAPH_WINDOW_HEIGHT, GRAPH_WINDOW_WIDTH, PARAMETERS_WINDOW_WIDTH
 from views.SimulationDataView import SimulationDataView
 from controllers.IdlenessesController import IdlenessController
 
 class SimulationDataController:
     def __init__(self, screen: pygame.Surface) -> None:
-        self._simulation_data_view = SimulationDataView(screen)
+        self._simulation_data_view = SimulationDataView(
+            screen.subsurface(
+                (GRAPH_WINDOW_WIDTH, 0, PARAMETERS_WINDOW_WIDTH, GRAPH_WINDOW_HEIGHT)
+            ))
         self._idleness_controller = IdlenessController([],self._simulation_data_view)
 
     def draw_simulation_data(self) -> None:
