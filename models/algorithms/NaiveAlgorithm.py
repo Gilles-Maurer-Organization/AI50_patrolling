6702b2c25f9e1ec7eaeeef18dc8203fbf85303b2
Arnaud Planchin <arnaud.planchin@laposte.net>
2024-12-16 10:04:10 +0100

    Delete files

--- models/algorithms/NaiveAlgorithm.py
+++
@@ -1,7 +0,0 @@
-from models.algorithms.Algorithm import Algorithm
-
-class NaiveAlgorithm(Algorithm):
-    def __init__(self):
-        super().__init__()
-        self.parameters = {}
-        self.name = "Naive Algorithm"
