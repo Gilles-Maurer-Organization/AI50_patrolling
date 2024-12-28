12d6f7c1437495baaa29fb1bd569ebb6a00a8520
Raphael PERRIN <56697513+DynaTiuM@users.noreply.github.com>
2024-12-28 13:36:17 +0100

    Fix: Textbox number deletion (#108)
    * fix: textboxes
    
    * fix: allowing decimals only for algorithm parameters

--- controllers/text_boxes/BaseTextBoxController.py
+++ controllers/text_boxes/BaseTextBoxController.py
@@ -106,7 +106,7 @@ class BaseTextBoxController:
         if event.key == pygame.K_BACKSPACE:
             model.handle_backspace()
         # If it's not the backspace key, it must be a digit key
-        elif event.unicode.isdigit() or event.unicode == '.':
+        elif event.unicode.isdigit():
             model.add_character(event.unicode)
 
     def is_text_box_text_completed(self, model: TextBox) -> bool:
@@ -117,7 +117,7 @@ class BaseTextBoxController:
         Returns:
             bool: True if completed, False otherwise.
         """
-        return model.default_text != model.text_content
+        return model.default_text != model.text_content or not model.first_input
 
     def is_text_box_hovered(self, event, text_box_view) -> bool:
         """
