5f3ec54feab52977c112fac3e28050a859ecfdb0
Eileen <100201798+Eileenj57@users.noreply.github.com>
2025-01-05 13:09:59 +0100

    fix: Docstring (#125)

--- models/Agent.py
+++ models/Agent.py
@@ -2,7 +2,26 @@ import numpy as np
 import pygame
 
 class Agent:
+    """
+    This class represents an agent that follows a predefined path
+    through a graph.
+
+    Attributes:
+        path (list): The sequence of node indices that defines the path.
+        current_index (int): The index of the current node in the path.
+        graph (Graph): The graph containing the nodes of the path.
+        x (float): The x coordinate of the agent's current position.
+        y (float): The y coordinate of the agent's current position.
+        speed (float): The speed at which the agent moves.
+    """
     def __init__(self, path, graph):
+        """
+        Initializes an agent with a specified path and graph.
+
+        Args:
+            path (list): The path as a list of node indices.
+            graph (Graph): The graph containing the nodes.
+        """
         self.path = path
         self.current_index = 0
         self.graph = graph
@@ -12,11 +31,15 @@ class Agent:
         self.speed = 2
 
     def move(self):
-        '''
-        Déplace l'agent vers le prochain noeud dans le chemin.
-        '''
+        """
+        Moves the agent toward the next node in the path.
+
+        If the agent reaches the target node, it moves to the next
+        node in the path. The movement is normalized to ensure smooth
+        progression at the specified speed.
+        """
         if self.current_index < len(self.path) - 1:
-            # Déplacer vers le prochain nœud
+            # Move to the next node
             target_node = self.graph.nodes[self.path[self.current_index + 1]]
             target_x, target_y = target_node.x, target_node.y
             dx = target_x - self.x
@@ -24,17 +47,18 @@ class Agent:
             distance = np.sqrt(dx ** 2 + dy ** 2)
 
             if distance != 0:
-                # Normaliser le déplacement par rapport à la distance et appliquer la vitesse
+                # Normalize the movement by distance and apply speed
                 self.x += (dx / distance) * self.speed
                 self.y += (dy / distance) * self.speed
 
-            # Vérifier si l'agent a atteint le nœud cible
+            # Check if the agent has reached his target node
             if distance < self.speed:
                 self.current_index += 1
+
     def reset_path(self):
-        '''
-        Réinitialise le chemin de l'agent pour recommencer depuis le premier nœud.
-        '''
+        """
+        Resets the agent's path to start again from the first node.
+        """
         self.current_index = 0
         start_node = self.graph.nodes[self.path[self.current_index]]
         self.x, self.y = start_node.x, start_node.y
